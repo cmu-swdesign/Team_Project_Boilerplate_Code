42892b2e66814b80071e797e093c3a0b1f50c114
Shivam Bansal <shivamb@andrew.cmu.edu>
2025-02-06 18:07:01 -0500

    fixing ip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ const Register = () => {
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const navigate = useNavigate();
-    const vm_ip_addr = "http://128.2.205.21:5001/"	
+    const vm_ip_addr = "http://127.0.0.1:5001/"	
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -44,7 +44,7 @@ const Register = () => {
 
 const Home = () => {
     const [timeSlots, setTimeSlots] = useState([]);
-    const vm_ip_addr = "http://128.2.205.21:5001/"
+    const vm_ip_addr = "http://127.0.0.1:5001/"
     useEffect(() => {
         axios.get(vm_ip_addr + "time-slots")
             .then(response => setTimeSlots(response.data))
